a69087bcdef7fd7786d03c8e8123caf552efaf5f
art33 <12asdert345@gmail.com>
2019-10-22 00:18:17 -0700

    Add collapse to navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,59 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-const NavBar = ({ user }) => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Vidly
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+class NavBar extends Component {
+  state = {};
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <div className="navbar-nav mr-auto">
-          <NavLink className="nav-item nav-link" to="/movies">
-            Movies
-          </NavLink>
-          <NavLink className="nav-item nav-link" to="/customers">
-            Customers
-          </NavLink>
-          <NavLink className="nav-item nav-link" to="/rentals">
-            Rentals
-          </NavLink>
-          {!user && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/login">
-                Login
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/register">
-                Register
-              </NavLink>
-            </React.Fragment>
-          )}
-          {user && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/profile">
-                {user.name}
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/logout">
-                Logout
-              </NavLink>
-            </React.Fragment>
-          )}
+  constructor(props) {
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    const collapsed = this.state.collapsed;
+    const classOne = collapsed
+      ? 'collapse navbar-collapse'
+      : 'collapse navbar-collapse show';
+    const classTwo = collapsed
+      ? 'navbar-toggler navbar-toggler-right collapsed'
+      : 'navbar-toggler navbar-toggler-right';
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Vidly
+        </Link>
+        <button
+          onClick={this.toggleNavbar}
+          className={`${classTwo}`}
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className={`${classOne}`} id="navbarResponsive">
+          <div className="navbar-nav mr-auto">
+            <NavLink className="nav-item nav-link" to="/movies">
+              Movies
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/customers">
+              Customers
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/rentals">
+              Rentals
+            </NavLink>
+            {!this.props.user && (
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/login">
+                  Login
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/register">
+                  Register
+                </NavLink>
+              </React.Fragment>
+            )}
+            {this.props.user && (
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/profile">
+                  {this.props.user.name}
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/logout">
+                  Logout
+                </NavLink>
+              </React.Fragment>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
